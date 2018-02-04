d74068052a06562c0b2b2329b5e6d3e8a46e126a
bright <bright@gmail.com>
2018-02-04 23:19:02 +0800

    install pyim-basedict.

--- .emacs.d/elisp/init-3rd-party.el
+++ .emacs.d/elisp/init-3rd-party.el
@@ -177,14 +177,13 @@
 
 ;;; Pyim - "Chinese Pinyin Input Method"
 ;; "C-\" => toggle-input-method
-(require-package 'pyim)
-(require 'pyim)
-(require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
-(pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
-;; (setq default-input-method "pyim")
-(defun hydra-pyim-start ()
-  (interactive)
-  (pyim-start "pyim"))
+(when (and (maybe-require-package 'pyim)
+           (maybe-require-package 'pyim-basedict))
+  (require 'pyim)
+  (pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
+  (defun hydra-pyim-start ()
+    (interactive)
+    (pyim-start "pyim")))
 
 
 ;;; Beacon — Never lose your cursor again
