ec1d84bdc435258421555c925797cff61860c584
Huming Chen <huming.chen@ericsson.com>
2018-03-13 04:11:48 +0100

    Fix broken thing.
    Change-Id: I4d52fa53c342ebc481aaecba796ee7bad279d79a

--- .emacs.d/elisp/init-windows.el
+++ .emacs.d/elisp/init-windows.el
@@ -3,7 +3,6 @@
 ;;----------------------------------------------------------------------------
 
 ;; navigate window layouts with "C-c <left>" and "C-c <right>"
-(require 'window)
 (add-hook 'after-init-hook 'winner-mode)
 
 
@@ -20,17 +19,14 @@
 ;; When splitting window, show (other-buffer) in the new window
 ;;----------------------------------------------------------------------------
 (defun split-window-func-with-other-buffer (split-function)
-  (lambda (&optional arg)
-    "Split this window and switch to the new window unless ARG is provided."
-    (interactive "P")
-    (funcall split-function)
-    (let ((target-window (next-window)))
-      (set-window-buffer target-window (other-buffer))
-      (unless arg
-        (select-window target-window)))))
+  (lexical-let ((s-f split-function))
+    (lambda ()
+      (interactive)
+      (funcall s-f)
+      (set-window-buffer (next-window) (other-buffer)))))
 
-(global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer #'split-window-vertically))
-(global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer #'split-window-horizontally))
+(global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer 'split-window-vertically))
+(global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer 'split-window-horizontally))
 
 (defun sanityinc/toggle-delete-other-windows ()
   "Delete other windows in frame if any, or restore previous window config."
