aa77ba89982febdbad2b1396f893ad9c6aaa8fb7
陈虎明 <chenhuming@gmail.com>
2018-03-13 15:48:23 +0800

    Create init-http.el

---
+++ .emacs.d/elisp/init-http.el
@@ -0,0 +1,17 @@
+;;----------------------------------------------------------------------------
+;; http setting
+;;----------------------------------------------------------------------------
+
+(maybe-require-package 'httprepl)
+(when (maybe-require-package 'restclient)
+  (add-auto-mode 'restclient-mode "\\.rest\\'")
+
+  (defun sanityinc/restclient ()
+    (interactive)
+    (with-current-buffer (get-buffer-create "*restclient*")
+      (restclient-mode)
+      (pop-to-buffer (current-buffer)))))
+
+
+(provide 'init-http)
+;;; init-http.el ends here
