3cfdce6bb1797e5d80ad2cd24f262f9f07ab7998
陈虎明 <chenhuming@gmail.com>
2018-04-03 10:45:05 +0800

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,20 +119,11 @@
 
 
 ;;; config modeline format
-(setq display-time-format "[%A %Y/%m/%d %H:%M Time-Zone:'%Z']"
-      display-time-interval 60
-      display-time-default-load-average nil
-      display-time-mail-face 'custom-themed)
-
-;; display time
 (display-time-mode t);; config time format
 (setq display-time-format "[%A %Y/%m/%d %H:%M Time-Zone:'%Z']"
       display-time-interval 60
       display-time-default-load-average nil
       display-time-mail-face 'custom-themed)
-
-;; display time
-(display-time-mode t)
 
 
 ;;; auto-save settings
