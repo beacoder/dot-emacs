a90fe14d860c217b2ebdd73c4622a7c9cb4f7930
陈虎明 <chenhuming@gmail.com>
2018-04-18 11:13:39 +0800

    Update init-nxml.el

--- .emacs.d/elisp/init-nxml.el
+++ .emacs.d/elisp/init-nxml.el
@@ -16,27 +16,23 @@
 
 
 ;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
-(defun sanityinc/pp-xml-region (beg end)
-  "Pretty format XML markup in region. The function inserts
-linebreaks to separate tags that have nothing but whitespace
-between them.  It then indents the markup by using nxml's
-indentation rules."
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
   (interactive "r")
-  (unless (use-region-p)
-    (setq beg (point-min)
-          end (point-max)))
-  ;; Use markers because our changes will move END
-  (setq beg (set-marker (make-marker) beg)
-        end (set-marker (make-marker) end))
   (save-excursion
-    (goto-char beg)
-    (while (search-forward-regexp "\>[ \\t]*\<" end t)
-      (backward-char) (insert "\n"))
-    (nxml-mode)
-    (indent-region beg end)))
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
 
 ;; prettify the xml in the active region
-(defalias 'pt 'sanityinc/pp-xml-region)
+(defalias 'pt 'bf-pretty-print-xml-region)
 
 ;;----------------------------------------------------------------------------
 ;; Integration with tidy for html + xml
