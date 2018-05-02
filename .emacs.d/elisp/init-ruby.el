b38e658be7b1b026974e8432558f59a4cbc932c3
陈虎明 <chenhuming@gmail.com>
2018-05-02 10:00:18 +0800

    Update init-ruby.el

--- .emacs.d/elisp/init-ruby.el
+++ .emacs.d/elisp/init-ruby.el
@@ -144,12 +144,7 @@
 
 (when (maybe-require-package 'projectile-rails)
   (add-hook 'projectile-mode-hook
-            (lambda () (projectile-rails-global-mode projectile-mode)))
-  (after-load 'projectile
-    (after-load 'guide-key
-      (add-to-list 'guide-key/guide-key-sequence "C-c r")
-      (add-to-list 'guide-key/guide-key-sequence "C-c r !")
-      (add-to-list 'guide-key/guide-key-sequence "C-c r g"))))
+            (λ () (projectile-rails-global-mode projectile-mode))))
 
 
 (provide 'init-ruby)
