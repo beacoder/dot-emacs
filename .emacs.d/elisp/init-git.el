e17cbe452786cb735a1a6afe2b1fe6d995864863
陈虎明 <chenhuming@gmail.com>
2018-05-22 16:33:55 +0800

    Update init-git.el

--- .emacs.d/elisp/init-git.el
+++ .emacs.d/elisp/init-git.el
@@ -17,6 +17,7 @@
 (require-package 'git-timemachine)
 
 
+;; "magit-blame"         => for each line show the revision in which it was added
 (when (maybe-require-package 'magit)
   (setq-default magit-diff-refine-hunk t)
 
