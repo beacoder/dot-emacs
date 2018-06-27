28e7602d18ca536d37141be51c457e49a0aab4e4
陈虎明 <chenhuming@gmail.com>
2018-06-27 11:08:16 +0800

    format function quotation.

--- .emacs.d/elisp/init-ibuffer.el
+++ .emacs.d/elisp/init-ibuffer.el
@@ -62,6 +62,7 @@
 
 (setq ibuffer-filter-group-name-face 'font-lock-doc-face)
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-x C-b") #'ibuffer)
 
 (provide 'init-ibuffer)
+;;; init-ibuffer.el ends here
