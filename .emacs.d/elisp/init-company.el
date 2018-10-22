27ddf2aeb3b637296f1f71794e7fb24aa3784e9e
陈虎明 <chenhuming@gmail.com>
2018-10-22 10:41:25 +0800

    diminish company-mode

--- .emacs.d/elisp/init-company.el
+++ .emacs.d/elisp/init-company.el
@@ -10,8 +10,8 @@
 
 (when (maybe-require-package 'company)
   (add-hook 'after-init-hook 'global-company-mode)
-  (after-load 'company
-    (diminish 'company-mode "CMP")
+  (after-load 'company    
+    (diminish 'company-mode)
     ;; (define-key company-mode-map (kbd "M-/") 'company-complete)
     ;; (define-key company-active-map (kbd "M-/") 'company-select-next)
     (setq-default company-backends
