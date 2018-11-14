fb625ec46448e56bba967cdd02d9dbbea05b4450
陈虎明 <chenhuming@gmail.com>
2018-11-14 12:34:01 +0800

    Add transpose-windows

--- .emacs.d/elisp/init-windows.el
+++ .emacs.d/elisp/init-windows.el
@@ -91,6 +91,21 @@ Call a second time to restore the original window configuration."
              (buffer-name))))
 
 
+;; @see https://www.emacswiki.org/emacs/TransposeWindows
+(defun transpose-windows ()
+  "Transpose two windows.  If more or less than two windows are visible, error."
+  (interactive)
+  (unless (= 2 (count-windows))
+    (error "There are not 2 windows."))
+  (let* ((windows (window-list))
+         (w1 (car windows))
+         (w2 (nth 1 windows))
+         (w1b (window-buffer w1))
+         (w2b (window-buffer w2)))
+    (set-window-buffer w1 w2b)
+    (set-window-buffer w2 w1b)))
+
+
 (unless (memq window-system '(nt w32))
   (windmove-default-keybindings 'control))
 
