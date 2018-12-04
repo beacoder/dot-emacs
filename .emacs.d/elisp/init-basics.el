1e79611d16920419bd978d4602c3033ed61b0fe8
陈虎明 <chenhuming@gmail.com>
2018-12-04 11:07:12 +0800

    auto-refresh imenu items after auto-revert

--- .emacs.d/elisp/init-basics.el
+++ .emacs.d/elisp/init-basics.el
@@ -83,8 +83,8 @@ The argument has the same meaning as in `apropos'."
 
 ;; auto-refresh all buffers when files have changed on disk.
 (add-hook 'after-init-hook 'global-auto-revert-mode)
-;; auto-refresh imenu items
-(add-hook 'global-auto-revert-mode-hook
+;; auto-refresh imenu items after auto-revert
+(add-hook 'after-revert-hook
           #'(λ () (when (derived-mode-p 'prog-mode)
                     (setq imenu--index-alist nil))))
 ;; I use this mode for log files (Emacs’s version of tail -f) to easily search through the logs.
