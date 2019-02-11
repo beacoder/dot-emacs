26d3aad4920974d8db14ae8acc161f215894920c
陈虎明 <chenhuming@gmail.com>
2019-02-11 10:26:06 +0800

    Use exec-path-from-shell when running as a daemon (except on Windows).

--- .emacs.d/elisp/init-shell.el
+++ .emacs.d/elisp/init-shell.el
@@ -9,7 +9,9 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "GTAGSLIBPATH"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
-(when (memq window-system '(mac ns x))
+(when (or (memq window-system '(mac ns x))
+          (unless (memq system-type '(ms-dos windows-nt))
+            (daemonp)))
   (setq-default exec-path-from-shell-arguments nil)
   (exec-path-from-shell-initialize))
 
