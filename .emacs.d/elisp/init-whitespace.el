64fb51e42289b577cded86e3f88d3b0c08d1eef7
陈虎明 <chenhuming@gmail.com>
2019-02-19 17:20:06 +0800

    Update init-whitespace.el

--- .emacs.d/elisp/init-whitespace.el
+++ .emacs.d/elisp/init-whitespace.el
@@ -1,32 +1,25 @@
-;;----------------------------------------------------------------------------
-;; whitespace setting
-;;----------------------------------------------------------------------------
+;;; init-whitespace.el --- Special handling for whitespace -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
 ;;; "M-SPC" => cycle-spacing
 (global-set-key [remap just-one-space] #'cycle-spacing)
 
+(setq-default show-trailing-whitespace nil)
+
 
 ;;; Whitespace
-(setq-default show-trailing-whitespace t)
-(defun sanityinc/no-trailing-whitespace ()
-  "Turn off display of trailing whitespace in this buffer."
-  (setq show-trailing-whitespace nil))
 
-;; But don't show trailing whitespace in SQLi, inf-ruby etc.
-(dolist (hook '(special-mode-hook
-                Info-mode-hook
-                eww-mode-hook
-                term-mode-hook
-                comint-mode-hook
-                compilation-mode-hook
-                twittering-mode-hook
-                minibuffer-setup-hook
-                ttcn-3-mode-hook))
-  (add-hook hook #'sanityinc/no-trailing-whitespace))
+(defun sanityinc/show-trailing-whitespace ()
+  "Enable display of trailing whitespace in this buffer."
+  (setq-local show-trailing-whitespace t))
+
+(dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
+  (add-hook hook 'sanityinc/show-trailing-whitespace))
 
 
 (require-package 'whitespace-cleanup-mode)
-(add-hook 'after-init-hook #'global-whitespace-cleanup-mode)
+(add-hook 'after-init-hook 'global-whitespace-cleanup-mode)
 (after-load 'whitespace-cleanup-mode
   (diminish 'whitespace-cleanup-mode))
 
