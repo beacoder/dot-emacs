c21509b9568870bd9df30b7b8db7352950633243
陈虎明 <chenhuming@gmail.com>
2019-03-08 09:29:55 +0800

    Update init-register.el

--- .emacs.d/elisp/init-register.el
+++ .emacs.d/elisp/init-register.el
@@ -9,9 +9,9 @@
 
 (set-register ?e '(file . "~/.emacs.d/elisp"))
 
-(set-register ?i '(file . "~/.emacs.d/init.el"))
-
 (set-register ?h '(file . "~/.bash_history"))
+
+(set-register ?i '(file . "~/.emacs.d/init.el"))
 
 (set-register ?o '(file . "~/.emacs.d/tutorials/org-tutorial.org"))
 
