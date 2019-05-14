6416cb3397d2809a6e262746c910287d821faf7f
陈虎明 <chenhuming@gmail.com>
2019-05-14 10:22:52 +0800

    Config org-agenda-files.

--- .emacs.d/elisp/init-org.el
+++ .emacs.d/elisp/init-org.el
@@ -48,7 +48,8 @@
       org-fast-tag-selection-single-key 'expert
       org-html-validation-link nil
       org-export-kill-product-buffer-when-displayed t
-      org-tags-column 80)
+      org-tags-column 80
+      org-agenda-files '("~/workspace/todo.org"))
 
 
 ;; Lots of stuff from http://doc.norang.ca/org-mode.html
