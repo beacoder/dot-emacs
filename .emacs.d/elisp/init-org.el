23002fc995af38b98b630bfaf229a4890eec2597
陈虎明 <chenhuming@gmail.com>
2021-03-11 20:36:52 +0800

    Add support for preview org agenda item.

--- .emacs.d/elisp/init-org.el
+++ .emacs.d/elisp/init-org.el
@@ -392,6 +392,9 @@ typical word processor."
 ;;; Incremental org-search-vew
 ;;; Begin
 
+(defvar smart/ivy-index-to-agenda-item-list
+  "The alist to store mapping from ivy-index to agenda-item.")
+
 (defun smart/org-search-view ()
   "Incremental `org-search-view'."
   (interactive)
@@ -418,7 +421,9 @@ typical word processor."
                                (and (file-exists-p a)
                                     (file-exists-p b)
                                     (file-equal-p a b))))
-                rtnall nil)
+                rtnall nil
+                index 0
+                smart/ivy-index-to-agenda-item-list nil)
           ;; loop agenda files to find matched one
           (while (setq file (pop files))
             (setq ee nil)
@@ -428,6 +433,7 @@ typical word processor."
               (with-temp-buffer
                 (insert-file-contents-literally file)
                 (org-mode)
+                ;; clear ivy-index to agenda-item mapping
                 (with-syntax-table (org-search-syntax-table)
                   (let ((case-fold-search t))
                     (widen)
@@ -437,6 +443,7 @@ typical word processor."
                       (throw 'nextfile t))
                     (goto-char (max (point-min) (1- (point))))
                     ;; real match happens here
+
                     (while (re-search-forward string nil t)
                       (org-back-to-heading t)
                       (while (and (not (zerop org-agenda-search-view-max-outline-level))
@@ -457,10 +464,15 @@ typical word processor."
                                   (point)))
                       (goto-char beg)
                       ;; save found text and its location
-                      (setq txt (propertize (buffer-substring-no-properties beg1 (point-at-eol))
-                                            'location (format "%s:%d" file (line-number-at-pos beg))))
+                      (setq txt
+                            (propertize (buffer-substring-no-properties beg1 (point-at-eol))
+                                        'location (format "%s:%d" file (line-number-at-pos beg))))
+                      ;; save in map
+                      (push (cons index txt) smart/ivy-index-to-agenda-item-list)
+                      ;; save in return value
                       (push txt ee)
-                      (goto-char (1- end)))))))
+                      (goto-char (1- end))
+                      (setq index (1+ index)))))))
             (setq rtn (nreverse ee))
             (setq rtnall (append rtnall rtn)))
           rtnall))))
@@ -480,6 +492,24 @@ typical word processor."
              (is-valid-nb (integerp line-nb)))
     (find-file-read-only-other-window file-name)
     (with-no-warnings (goto-line line-nb))))
+
+(defun smart/org-iterate-action (&optional arg)
+  "Preview agenda content while looping agenda, ignore ARG."
+  (when-let ((is-map-valid smart/ivy-index-to-agenda-item-list)
+             (item-found (assoc ivy--index smart/ivy-index-to-agenda-item-list))
+             (item-content (cdr item-found))
+             (location (get-text-property 0 'location item-content)))
+    (save-mark-and-excursion
+      (smart/visit-agenda-location location)
+      (when (active-minibuffer-window)
+        (select-window (active-minibuffer-window))))))
+
+;; auto-refresh imenu items after auto-revert
+(advice-remove 'ivy-previous-line 'smart/org-iterate-action)
+(advice-add 'ivy-previous-line :after 'smart/org-iterate-action)
+
+(advice-remove 'ivy-next-line 'smart/org-iterate-action)
+(advice-add 'ivy-next-line :after 'smart/org-iterate-action)
 
 ;;; Incremental org-search-vew
 ;;; End
