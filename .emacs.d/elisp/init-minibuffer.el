077182e71dc188818d25308bd2affd5903f10cac
陈虎明 <chenhuming@gmail.com>
2021-05-27 11:12:20 +0800

    Add init-minibuffer.el

---
+++ .emacs.d/elisp/init-minibuffer.el
@@ -0,0 +1,56 @@
+;;; init-minibuffer.el --- Config for minibuffer completion       -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+
+(when (maybe-require-package 'vertico)
+  (add-hook 'after-init-hook 'vertico-mode)
+
+  (require-package 'orderless)
+  (with-eval-after-load 'vertico
+    (require 'orderless))
+
+  (setq completion-styles '(substring orderless))
+
+  (when (maybe-require-package 'embark)
+    (with-eval-after-load 'vertico
+      (define-key vertico-map (kbd "C-c C-o") 'embark-export)
+      (define-key vertico-map (kbd "C-c C-c") 'embark-act)))
+
+  (when (maybe-require-package 'consult)
+    (when (maybe-require-package 'projectile)
+      (require 'projectile)
+      (setq-default consult-project-root-function 'projectile-project-root))
+
+    (when (executable-find "rg")
+      (maybe-require-package 'affe)
+      (defun sanityinc/affe-grep-at-point (&optional dir initial)
+        (interactive (list prefix-arg (when-let ((s (symbol-at-point)))
+                                        (symbol-name s))))
+        (affe-grep dir initial))
+      (global-set-key (kbd "M-?") 'sanityinc/affe-grep-at-point))
+
+    (global-set-key [remap switch-to-buffer] 'consult-buffer)
+    (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
+    (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
+    (global-set-key [remap goto-line] 'consult-goto-line)
+
+    (with-eval-after-load 'consult
+      (dolist (cmd '(consult-ripgrep affe-grep sanityinc/affe-grep-at-point))
+        (add-to-list 'consult-config
+                     `(,cmd :preview-key ,(kbd "M-P")))))
+
+    (when (maybe-require-package 'embark-consult)
+      (with-eval-after-load 'embark
+        (require 'embark-consult)
+        (add-hook 'embark-collect-mode-hook 'embark-consult-preview-minor-mode)))
+
+    (maybe-require-package 'consult-flycheck)))
+
+(when (maybe-require-package 'marginalia)
+  (add-hook 'after-init-hook 'marginalia-mode)
+  (setq-default marginalia-annotators '(marginalia-annotators-heavy)))
+
+
+(provide 'init-minibuffer)
+;;; init-minibuffer.el ends here
