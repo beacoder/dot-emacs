0ed300f60baec97173dfb6104e20e562901d75b1
陈虎明 <chenhuming@gmail.com>
2021-06-28 09:56:48 +0800

    Add sanityinc/no-consult-preview.

--- .emacs.d/elisp/init-minibuffer.el
+++ .emacs.d/elisp/init-minibuffer.el
@@ -20,27 +20,33 @@
       (define-key vertico-map (kbd "C-c C-c") 'embark-act)))
 
   (when (maybe-require-package 'consult)
+    (defmacro sanityinc/no-consult-preview (&rest cmds)
+      `(with-eval-after-load 'consult
+         (consult-customize ,@cmds :preview-key (kbd "M-P"))))
+
+    (sanityinc/no-consult-preview
+     consult-ripgrep
+     consult-git-grep consult-grep
+     consult-bookmark consult-recent-file consult-xref
+     consult--source-file consult--source-project-file consult--source-bookmark)
+
     (when (maybe-require-package 'projectile)
       (require 'projectile)
       (setq-default consult-project-root-function 'projectile-project-root))
 
-    (when (executable-find "rg")
-      (maybe-require-package 'affe)
+    (when (and (executable-find "rg") (maybe-require-package 'affe))
       (defun sanityinc/affe-grep-at-point (&optional dir initial)
         (interactive (list prefix-arg (when-let ((s (symbol-at-point)))
                                         (symbol-name s))))
         (affe-grep dir initial))
-      (global-set-key (kbd "M-?") 'sanityinc/affe-grep-at-point))
+      (global-set-key (kbd "M-?") 'sanityinc/affe-grep-at-point)
+      (sanityinc/no-consult-preview sanityinc/affe-grep-at-point)
+      (with-eval-after-load 'affe (sanityinc/no-consult-preview affe-grep)))
 
     (global-set-key [remap switch-to-buffer] 'consult-buffer)
     (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
     (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
     (global-set-key [remap goto-line] 'consult-goto-line)
-
-    (with-eval-after-load 'consult
-      (dolist (cmd '(consult-ripgrep affe-grep sanityinc/affe-grep-at-point))
-        (add-to-list 'consult-config
-                     `(,cmd :preview-key ,(kbd "M-P")))))
 
     (when (maybe-require-package 'embark-consult)
       (with-eval-after-load 'embark
