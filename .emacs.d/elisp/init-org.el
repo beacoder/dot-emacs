dfc3ac006fde527a6b805ae6f00ef19c0e5e0fa4
陈虎明 <chenhuming@gmail.com>
2021-08-09 10:09:41 +0800

    Add org timer hot key reminder.

--- .emacs.d/elisp/init-org.el
+++ .emacs.d/elisp/init-org.el
@@ -24,12 +24,16 @@
 
 ;;; useful key-bindings
 ;;----------------------------------------------------------------------------
-;;  org-agenda   (C-c a)
-;;  org-capture  (C-c c)
-;;  org-todo     (C-c C-t)
-;;  org-priority (C-c ,)
-;;  org-schedule (C-c C-s) e.g: +/- 1d/1w
-;;  org-deadline (C-c C-d) e.g: +/- 1d/1w
+;; org-agenda   (C-c a)
+;; org-capture  (C-c c)
+;; org-todo     (C-c C-t)
+;; org-priority (C-c ,)
+;; org-schedule (C-c C-s) e.g: +/- 1d/1w
+;; org-deadline (C-c C-d) e.g: +/- 1d/1w
+
+;; org-timer-start     (C-c C-x 0) count-up timer
+;; org-timer-set-timer (C-c C-x ;) count-down timer
+;; org-timer-stop      (C-c C-x _) stop timer
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Org table
