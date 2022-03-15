1cc7e9456c3e661458c4d75ba013bc9e9c241cf6
陈虎明 <chenhuming@gmail.com>
2022-03-15 16:50:37 +0800

    Update init-hydra.el

--- .emacs.d/elisp/init-hydra.el
+++ .emacs.d/elisp/init-hydra.el
@@ -68,7 +68,7 @@
 [_a_] Counsel-Ag         [_g_] Counsel-Git-Grep [_f_] Counsel-Git      [_l_] Counsel-Locate     [_P_] Move-Text-Up
 [_u_] Update-GTAGS       [_c_] Mode-Compile     [_C_] Compile          [_r_] Recompile          [_N_] Move-Text-Down
 [_p_] Previous-Mark      [_n_] Next-Mark        [_s_] Sort-Lines       [_d_] Remove-Duplicate   [_o_] Org-Search-View
-[_j_] Dumb-Jump          [_i_] Pin-Yin          [_w_] Google-Word      [_k_] Google-Lucky       [_H_] Hs-Show-Block
+[_j_] Dumb-Jump          [_i_] Pin-Yin          [_w_] Google-Word      [_h_] Hs-Hide-Block      [_H_] Hs-Show-Block
 [_q_] Quit
   "
   ("a" smart/counsel-ag :exit t)
