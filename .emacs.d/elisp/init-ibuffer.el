6e027efc6b7fa4cb106325a61f4c8c448ee76a56
陈虎明 <chenhuming@gmail.com>
2022-04-02 15:19:53 +0800

    Add header.

--- .emacs.d/elisp/init-ibuffer.el
+++ .emacs.d/elisp/init-ibuffer.el
@@ -1,5 +1,10 @@
+;;; init-ibuffer.el --- ibuffer settings -*- lexical-binding: t -*-
+;;; Commentary:
+
 ;; TODO: enhance ibuffer-fontification-alist
 ;;   See http://www.reddit.com/r/emacs/comments/21fjpn/fontifying_buffer_list_for_emacs_243/
+
+;;; Code:
 
 (require-package 'fullframe)
 (with-eval-after-load 'ibuffer
