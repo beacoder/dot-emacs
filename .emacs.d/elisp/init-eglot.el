a907c95bff97983eb91e64123880d72f281346da
陈虎明 <chenhuming@gmail.com>
2023-01-07 10:02:12 +0800

    Add init-eglot.el

---
+++ .emacs.d/elisp/init-eglot.el
@@ -0,0 +1,27 @@
+;;; init-eglot.el --- LSP support via eglot -*- lexical-binding: t -*-
+;;; Commentary:
+;;;
+;;; LSP support via eglot
+;;;
+
+;;; Code:
+
+;;; useful key-bindings
+;;----------------------------------------------------------------------------
+;;; eglot
+;;  xref-find-definitions  (M-.)
+;;  xref-find-references   (M-])
+;;  complete-symbol        (C-M-i)
+;;  eglot-rename
+
+
+;; For Emacs >= 27
+(setq read-process-output-max (* 1024 1024))
+
+
+(when (maybe-require-package 'eglot)
+  (maybe-require-package 'consult-eglot))
+
+
+(provide 'init-eglot)
+;;; init-eglot.el ends here
