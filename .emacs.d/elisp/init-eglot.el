ac5f449b4030151998952eeffd21584356042ede
陈虎明 <chenhuming@gmail.com>
2023-01-07 10:04:39 +0800

    Update init-eglot.el

--- .emacs.d/elisp/init-eglot.el
+++ .emacs.d/elisp/init-eglot.el
@@ -9,11 +9,10 @@
 ;;; useful key-bindings
 ;;----------------------------------------------------------------------------
 ;;; eglot
-;;  xref-find-definitions  (M-.)
-;;  xref-find-references   (M-])
-;;  complete-symbol        (C-M-i)
+;;  xref-find-definitions (M-.)
+;;  xref-find-references  (M-])
+;;  complete-symbol       (C-M-i)
 ;;  eglot-rename
-
 
 ;; For Emacs >= 27
 (setq read-process-output-max (* 1024 1024))
