3c0e30f6a4fe2d8cf6d1b5fb1c52063148b802c5
陈虎明 <chenhuming@gmail.com>
2023-01-18 00:32:51 +0800

    Remove non-functioning hotkey bindings.

--- .emacs.d/elisp/init-cc.el
+++ .emacs.d/elisp/init-cc.el
@@ -61,8 +61,6 @@
 
   ;; navigation between header and cpp/cc files
   (local-set-key (kbd "C-c o") #'ff-find-other-file)
-  (local-set-key (kbd "C-M-a") #'c-beginning-of-defun)
-  (local-set-key (kbd "C-M-e") #'c-end-of-defun)
 
   ;; we like auto-newline and hungry-delete
   (c-toggle-auto-hungry-state 1)
