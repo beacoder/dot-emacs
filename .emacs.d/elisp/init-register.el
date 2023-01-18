69e50804c8886e001701af508e5d2fc4c202f242
陈虎明 <chenhuming@gmail.com>
2023-01-18 12:15:23 +0800

    Update init-register.el

--- .emacs.d/elisp/init-register.el
+++ .emacs.d/elisp/init-register.el
@@ -8,17 +8,13 @@
   ;; <C-x r j b> => open directory "~/backup"
   (set-register ?b '(file . "~/backup"))
 
-  (set-register ?c '(file . "~/.bashrc"))
+  (set-register ?c '(file . "~/private/init.csh"))
 
   (set-register ?e '(file . "~/.emacs.d/elisp"))
 
-  (set-register ?h '(file . "~/.bash_history"))
-
-  (set-register ?i '(file . "~/.emacs.d/init.el"))
+  (set-register ?h '(file . "~/.sh_history"))
 
   (set-register ?o '(file . "~/.emacs.d/tutorials/org-tutorial.org"))
-
-  (set-register ?t '(file . "~/workspace/org/todo.org"))
 
   (set-register ?w '(file . "~/workspace")))
 
