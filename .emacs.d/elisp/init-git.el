0f52fa090fc0a8faeca79730f699f6aeebe2caa4
陈虎明 <chenhuming@gmail.com>
2023-08-01 16:09:33 +0800

    add hotkey for magit-blame-addition.

--- .emacs.d/elisp/init-git.el
+++ .emacs.d/elisp/init-git.el
@@ -21,15 +21,16 @@
 (require-package 'git-link)
 
 
-;; "magit-blame-addition" => for each line show the revision in which it was added
-;; "C-x v p"              => last commit for current thing-at-point
-;; "C-x v l"              => git history of current file
-;; "C-x v t"              => git timemachine
+;; "C-x v b" => for each line show the revision in which it was added
+;; "C-x v p" => last commit for current thing-at-point
+;; "C-x v l" => git history of current file
+;; "C-x v t" => git timemachine
 (when (maybe-require-package 'magit)
   (setq-default magit-diff-refine-hunk 'all)
 
   (global-set-key (kbd "C-x g") #'magit-status)
   (global-set-key (kbd "C-x M-g") #'magit-dispatch)
+  (global-set-key (kbd "C-x v b") #'magit-blame-addition)
 
   (defun sanityinc/magit-or-vc-log-file (&optional prompt)
     (interactive "P")
