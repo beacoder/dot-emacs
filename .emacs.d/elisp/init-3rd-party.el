b8ac122ba9ca8878b83bc0399675765cb08abfba
陈虎明 <chenhuming@gmail.com>
2023-08-09 08:55:24 +0800

    enhance expand region with treesitter.

--- .emacs.d/elisp/init-3rd-party.el
+++ .emacs.d/elisp/init-3rd-party.el
@@ -191,8 +191,25 @@
 
 
 ;;; Expand region
-(require-package 'expand-region)
-(global-set-key (kbd "M-8") #'er/expand-region)
+(use-package expand-region
+  :ensure t
+  :bind ("M-8" . er/expand-region)
+  :config
+  (when (treesit-available-p)
+    (defun treesit-mark-bigger-node ()
+      "Use tree-sitter to mark regions."
+      (let* ((root (treesit-buffer-root-node))
+             (node (treesit-node-descendant-for-range root (region-beginning) (region-end)))
+             (node-start (treesit-node-start node))
+             (node-end (treesit-node-end node)))
+        ;; Node fits the region exactly. Try its parent node instead.
+        (when (and (= (region-beginning) node-start) (= (region-end) node-end))
+          (when-let ((node (treesit-node-parent node)))
+            (setq node-start (treesit-node-start node)
+                  node-end (treesit-node-end node))))
+        (set-mark node-end)
+        (goto-char node-start)))
+    (add-to-list 'er/try-expand-list 'treesit-mark-bigger-node)))
 
 
 ;;; Yaml mode
