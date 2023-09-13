0b7faeb86a831e91600a48d1c67ec66806e37a87
陈虎明 <chenhuming@gmail.com>
2023-09-13 11:04:40 +0800

    remove not used python library.

--- .emacs.d/elisp/init-python.el
+++ .emacs.d/elisp/init-python.el
@@ -19,36 +19,21 @@
 
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
-		("SConscript\\'" . python-mode))
-              auto-mode-alist)
-      python-shell-interpreter "python3")
+                ("SConscript\\'" . python-mode))
+              auto-mode-alist))
+
+(setq python-shell-interpreter "python3")
 
 (require-package 'pip-requirements)
 
-(when (maybe-require-package 'anaconda-mode)
-  (with-eval-after-load 'python
-    ;; Anaconda doesn't work on remote servers without some work, so
-    ;; by default we enable it only when working locally.
-    (add-hook 'python-mode-hook
-              (lambda () (unless (file-remote-p default-directory)
-                      (anaconda-mode 1))))
-    (bind-keys
-     :map python-mode-map
-     ("C-c c" . python-skeleton-class)
-     ("C-c d" . python-skeleton-def)
-     ("C-c f" . python-skeleton-for)
-     ("C-c i" . python-skeleton-if)
-     ("C-c m" . python-skeleton-import)
-     ("C-c t" . python-skeleton-try)
-     ("C-c w" . python-skeleton-while)
-     ("C-c C-f" . nil))) ;; allow global binding for "C-c C-f"
+(when (maybe-require-package 'toml-mode)
+  (add-to-list 'auto-mode-alist '("poetry\\.lock\\'" . toml-mode)))
 
-  (with-eval-after-load 'anaconda-mode
-    (bind-keys
-     :map anaconda-mode-map
-     ("M-]" . anaconda-mode-find-references)
-     ("M-=" . anaconda-mode-find-assignments)
-     ("M-?" . anaconda-mode-show-doc))))
+(when (maybe-require-package 'reformatter)
+  (reformatter-define black :program "black" :args '("-")))
+
+(provide 'init-python)
+;;; init-python.el ends here
 
 
 (provide 'init-python)
