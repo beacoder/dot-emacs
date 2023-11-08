5d94e7395adfedb74883016b0f7ab8bf5e9a3d76
陈虎明 <chenhuming@gmail.com>
2023-11-08 09:39:29 +0800

    Update init-eglot.el

--- .emacs.d/elisp/init-eglot.el
+++ .emacs.d/elisp/init-eglot.el
@@ -15,24 +15,9 @@
 ;;  complete-symbol       (C-M-i)
 ;;  eglot-rename
 
-;; For Emacs >= 27
-(setq read-process-output-max (* 1024 1024))
-
 
 (when (maybe-require-package 'eglot)
-  (maybe-require-package 'consult-eglot)
-  ;; disable eglot for now, since clangd index is not working
-  ;; ;; use eglot for all modes
-  ;; (use-package eglot
-  ;;   :hook ((prog-mode . (lambda () (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode)
-  ;;                               (eglot-ensure))))
-  ;;          ((markdown-mode yaml-mode) . eglot-ensure)))
-  ;; ;; use ccls instead of clangd for c++
-  ;; (with-eval-after-load 'eglot
-  ;;   (add-to-list 'eglot-server-programs
-  ;;                '((c-mode c++-mode)
-  ;;                  . ("/proj/epg-tools/ccls/dce86b13-clang13.0.0_2/bin/ccls"))))
-)
+  (maybe-require-package 'consult-eglot))
 
 
 (provide 'init-eglot)
