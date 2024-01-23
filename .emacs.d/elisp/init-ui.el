f34b566c7d652fc5805b85ab4041c4bc3bc671fd
陈虎明 <chenhuming@gmail.com>
2024-01-23 14:33:10 +0900

    replace maybe-require-package with use-package.

--- .emacs.d/elisp/init-ui.el
+++ .emacs.d/elisp/init-ui.el
@@ -7,37 +7,36 @@
 
 ;;; nerd-icons
 (when (display-graphic-p)
-  (when (maybe-require-package 'nerd-icons)
-    (use-package nerd-icons
-      :diminish
-      :config (nerd-icons-install-fonts t)))
+  (use-package nerd-icons
+    :ensure t
+    :diminish
+    :config (nerd-icons-install-fonts t))
 
   ;;; dired support
-  (when (maybe-require-package 'nerd-icons-dired)
-    (use-package nerd-icons-dired
-      :diminish
-      :custom-face
-      (nerd-icons-dired-dir-face ((t (:inherit nerd-icons-dsilver :foreground unspecified))))
-      :hook (dired-mode . nerd-icons-dired-mode)))
+  (use-package nerd-icons-dired
+    :ensure t
+    :diminish
+    :custom-face
+    (nerd-icons-dired-dir-face ((t (:inherit nerd-icons-dsilver :foreground unspecified))))
+    :hook (dired-mode . nerd-icons-dired-mode))
 
   ;;; region completion support
-  (when (maybe-require-package 'nerd-icons-corfu)
-    (use-package nerd-icons-corfu
-      :after corfu
-      :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)))
+  (use-package nerd-icons-corfu
+    :ensure t
+    :after corfu
+    :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
   ;;; ibuffer support
-  (when (maybe-require-package 'nerd-icons-ibuffer)
-    (use-package nerd-icons-ibuffer
-      :hook (ibuffer-mode . nerd-icons-ibuffer-mode)))
+  (use-package nerd-icons-ibuffer
+    :ensure t
+    :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
 
   ;;; ivy support
-  (when (maybe-require-package 'nerd-icons-ivy-rich)
-    (use-package nerd-icons-ivy-rich
-      :ensure t
-      :init
-      (nerd-icons-ivy-rich-mode 1)
-      (ivy-rich-mode 1))))
+  (use-package nerd-icons-ivy-rich
+    :ensure t
+    :init
+    (nerd-icons-ivy-rich-mode 1)
+    (ivy-rich-mode 1)))
 
 
 (provide 'init-ui)
