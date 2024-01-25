5f333843e61ed388cddcd43e49b5cce0e6aba971
陈虎明 <chenhuming@gmail.com>
2024-01-25 12:07:50 +0900

    replace maybe-require-package with use-package.

--- .emacs.d/elisp/init-flymake.el
+++ .emacs.d/elisp/init-flymake.el
@@ -2,38 +2,35 @@
 ;;; Commentary:
 ;;; Code:
 
-(maybe-require-package 'flymake "1.2.1")
-
-;; Use flycheck checkers with flymake, to extend its coverage
-(when (maybe-require-package 'flymake-flycheck)
-  ;; Disable flycheck checkers for which we have flymake equivalents
-  (with-eval-after-load 'flycheck
-    (setq-default
-     flycheck-disabled-checkers
-     (append (default-value 'flycheck-disabled-checkers)
-             '(emacs-lisp emacs-lisp-checkdoc emacs-lisp-package sh-shellcheck))))
-
-  (add-hook 'flymake-mode-hook 'flymake-flycheck-auto)
-  (add-hook 'prog-mode-hook 'flymake-mode)
-  (add-hook 'text-mode-hook 'flymake-mode))
-
-(with-eval-after-load 'flymake
-  ;; Provide some flycheck-like bindings in flymake mode to ease transition
-  (define-key flymake-mode-map (kbd "C-c ! l") 'flymake-show-buffer-diagnostics)
-  (define-key flymake-mode-map (kbd "C-c ! n") 'flymake-goto-next-error)
-  (define-key flymake-mode-map (kbd "C-c ! p") 'flymake-goto-prev-error)
-  (define-key flymake-mode-map (kbd "C-c ! c") 'flymake-start)
-
+(use-package flymake
+  :ensure t
+  :diminish
+  :functions my-elisp-flymake-byte-compile
+  :bind ("C-c f" . flymake-show-buffer-diagnostics)
+  :hook (prog-mode . flymake-mode)
+  :init (setq flymake-no-changes-timeout nil
+              flymake-fringe-indicator-position 'right-fringe)
+  :config
+  ;; Check elisp with ``load-path''
+  (defun my-elisp-flymake-byte-compile (fn &rest args)
+    ;; checkdoc-params: (fn args)
+    "Wrapper for `elisp-flymake-byte-compile'."
+    (let ((elisp-flymake-byte-compile-load-path
+           (append elisp-flymake-byte-compile-load-path load-path)))
+      (apply fn args)))
+  (advice-add 'elisp-flymake-byte-compile :around #'my-elisp-flymake-byte-compile)
   ;; Disable error prompts
   (defun flymake-error-no-popping (orig &rest args) (list))
   (advice-add 'flymake-error :around 'flymake-error-no-popping))
 
-(unless (version< emacs-version "28.1")
-  (setq eldoc-documentation-function 'eldoc-documentation-compose)
 
-  (add-hook 'flymake-mode-hook
-            (lambda ()
-              (add-hook 'eldoc-documentation-functions 'flymake-eldoc-function nil t))))
+(use-package sideline-flymake
+  :ensure t
+  :diminish sideline-mode
+  :hook (flymake-mode . sideline-mode)
+  :init (setq sideline-flymake-display-mode 'point
+              sideline-backends-right '(sideline-flymake)))
+
 
 (provide 'init-flymake)
 ;;; init-flymake.el ends here
