2316d39ad5d09bc30f378d6dfa8fe165fccfa113
陈虎明 <chenhuming@gmail.com>
2024-02-29 15:08:28 +0900

    Restore previous windows after compile finished.

--- .emacs.d/elisp/init-compile.el
+++ .emacs.d/elisp/init-compile.el
@@ -2,9 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq-default compilation-scroll-output t)
+(require 'init-elpa)
+(require 'ansi-color)
 
 (require-package 'alert)
+(maybe-require-package 'cmd-to-echo)
+
+(setq-default compilation-scroll-output t)
+
+(defvar sanityinc/last-compilation-buffer nil
+  "The last buffer in which compilation took place.")
+
+(defvar sanityinc/last-window-configuration nil
+  "The last window configuration in which compilation took place.")
+
+(defvar sanityinc/last-selected-window nil
+  "The last selected window in which compilation took place.")
 
 ;;; compilation setting
 (defun mode-compile ()
@@ -49,17 +62,17 @@
                              (is-exist (string-match "*compilation*" buf-name)))
                     ;; @see http://emacswiki.org/emacs/ModeCompile#toc2
                     (bury-buffer "*compilation*")
-                    (winner-undo)
-                    (message "NO COMPILATION ERRORS!"))))))
+                    (delete-window (get-buffer-window (get-buffer "*compilation*")))
+                    (set-window-configuration sanityinc/last-window-configuration)
+                    (select-window sanityinc/last-selected-window)
+                    (message "NO COMPILATION ERRORS!")))))
 
-(defvar sanityinc/last-compilation-buffer nil
-  "The last buffer in which compilation took place.")
-
-(with-eval-after-load 'compile
-  (defun sanityinc/save-compilation-buffer (&rest _)
+  (defun sanityinc/save-compilation-buffer-and-window (&rest _)
     "Save the compilation buffer to find it later."
-    (setq sanityinc/last-compilation-buffer next-error-last-buffer))
-  (advice-add 'compilation-start :after 'sanityinc/save-compilation-buffer)
+    (setq sanityinc/last-compilation-buffer next-error-last-buffer
+          sanityinc/last-window-configuration (current-window-configuration)
+          sanityinc/last-selected-window (frame-selected-window)))
+  (advice-add 'compilation-start :after 'sanityinc/save-compilation-buffer-and-window)
 
   (defun sanityinc/find-prev-compilation (orig &optional edit-command)
     "Find the previous compilation buffer, if present, and recompile there."
@@ -70,8 +83,13 @@
         (with-current-buffer sanityinc/last-compilation-buffer
           (funcall orig edit-command))
       (funcall orig edit-command)))
-  (advice-add 'recompile :around 'sanityinc/find-prev-compilation))
+  (advice-add 'recompile :around 'sanityinc/find-prev-compilation)
 
+  (require 'ansi-color)
+  (defun sanityinc/colourise-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  (add-hook 'compilation-filter-hook 'sanityinc/colourise-compilation-buffer))
 
 (defun sanityinc/shell-command-in-view-mode (start end command &optional output-buffer replace &rest other-args)
   "Put \"*Shell Command Output*\" buffers into view-mode."
@@ -79,16 +97,6 @@
     (with-current-buffer "*Shell Command Output*"
       (view-mode 1))))
 (advice-add 'shell-command-on-region :after 'sanityinc/shell-command-in-view-mode)
-
-
-(with-eval-after-load 'compile
-  (require 'ansi-color)
-  (defun sanityinc/colourise-compilation-buffer ()
-    (when (eq major-mode 'compilation-mode)
-      (ansi-color-apply-on-region compilation-filter-start (point-max))))
-  (add-hook 'compilation-filter-hook 'sanityinc/colourise-compilation-buffer))
-
-(maybe-require-package 'cmd-to-echo)
 
 
 (provide 'init-compile)
