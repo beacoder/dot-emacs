4ff57f7a680e3d5bd6cb7dedd38b5c25ff4fa331
陈虎明 <chenhuming@gmail.com>
2024-09-05 10:38:06 +0900

    git-commit is now part of magit, and shouldn't be installed directly

--- .emacs.d/elisp/init-git.el
+++ .emacs.d/elisp/init-git.el
@@ -69,8 +69,7 @@
 (with-eval-after-load 'magit
   (fullframe magit-status magit-mode-quit-window))
 
-(when (maybe-require-package 'git-commit)
-  (add-hook 'git-commit-mode-hook 'goto-address-mode))
+(add-hook 'git-commit-mode-hook 'goto-address-mode)
 
 
 (when *is-a-mac*
