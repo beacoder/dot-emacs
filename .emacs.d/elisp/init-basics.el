6b3ee097a7d9ab5d64c0f4d17d7b6d8110b746c9
陈虎明 <chenhuming@gmail.com>
2025-02-17 12:34:18 +0900

    Update init-basics.el

--- .emacs.d/elisp/init-basics.el
+++ .emacs.d/elisp/init-basics.el
@@ -454,12 +454,21 @@ Use in `isearch-mode-end-hook'."
 ;;----------------------------------------------------------------------------
 ;; Register settings
 ;;----------------------------------------------------------------------------
-(set-register ?b '(file . "~/backup")) ;; <C-x r j b> => open directory "~/backup"
-(set-register ?c '(file . "~/private/init.csh"))
-(set-register ?e '(file . "~/.emacs.d/elisp"))
-(set-register ?h '(file . "~/.sh_history"))
-(set-register ?o '(file . "~/.emacs.d/tutorials/org-tutorial.org"))
-(set-register ?w '(file . "~/workspace"))
+(defun setup-my-registers ()
+  "Setup my own registers."
+  ;; <C-x r j b> => open directory "~/backup"
+  (set-register ?b '(file . "~/backup"))
+  (set-register ?c '(file . "~/private/init.csh"))
+  (set-register ?e '(file . "~/.emacs.d/elisp"))
+  (set-register ?h '(file . "~/.sh_history"))
+  (set-register ?o '(file . "~/.emacs.d/tutorials/org-tutorial.org"))
+  (set-register ?w '(file . "~/workspace")))
+(add-hook 'after-init-hook #'setup-my-registers)
+
+
+;; remove the ELPA directory to eliminate any outdated packages it may contain.
+;; e.g: check gptel's load-path => (cl-find-if (lambda (dir) (string-match-p "gptel" dir)) load-path)
+(shell-command "rm -rf ~/.emacs.d/elpa")
 
 
 (provide 'init-basics)
