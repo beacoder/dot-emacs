ca6d231989edbdf390b03b2cd136cb7afd9c4f08
陈虎明 <chenhuming@gmail.com>
2025-03-09 11:59:35 +0800

    Refactor gptel-tools.

--- .emacs.d/elisp/init-gptel-tools.el
+++ .emacs.d/elisp/init-gptel-tools.el
@@ -2,30 +2,54 @@
 ;;; Commentary:
 ;;; Code:
 
-;; define tool-callbacks
-(defun my-gptel--read_file(filepath)
-  (with-temp-buffer
-    (insert-file-contents (expand-file-name filepath))
-    (buffer-string)))
+;; Define tool-callbacks
+;; @note return either a result or a message to inform the LLM
+(progn
+  (defun my-gptel--read_file(filepath)
+    (with-temp-message (format "Reading file: %s" filepath)
+      (with-temp-buffer
+        (insert-file-contents (expand-file-name filepath))
+        (buffer-string))))
 
-(defun my-gptel--read_files (filepaths)
-  (mapconcat
-   (lambda (filepath)
-     (concat (my-gptel--read_file filepath) "\n\n"))
-   filepaths))
+  (defun my-gptel--read_files (filepaths)
+    (mapconcat
+     (lambda (filepath)
+       (concat (my-gptel--read_file filepath) "\n\n"))
+     filepaths))
 
-(defun my-gptel--create_file(path filename content)
-  (let ((full-path (expand-file-name filename path)))
-    (with-temp-buffer
-      (insert content)
-      (write-file full-path))
-    (format "Created file %s in %s" filename path)))
+  (defun my-gptel--create_file(path filename content)
+    (let ((full-path (expand-file-name filename path)))
+      (with-temp-buffer
+        (insert content)
+        (write-file full-path))
+      (format "Created file %s in %s" filename path)))
 
-(defun my-gptel--run_script (script_program script_file script_args)
-  (let ((command
-         (concat script_program " " (expand-file-name script_file) " " script_args)))
-    (with-temp-message (format "Running script: %s" command)
-      (shell-command-to-string command))))
+  (defun my-gptel--run_script (script_program script_file script_args)
+    (let ((command
+           (concat script_program " " (expand-file-name script_file) " " script_args)))
+      (with-temp-message (format "Running script: %s" command)
+        (shell-command-to-string command))))
+
+  (defun my-gptel--append_to_buffer (buffer text)
+    (with-current-buffer (get-buffer-create buffer)
+      (save-excursion
+        (goto-char (point-max))
+        (insert text)))
+    (format "Appended text to buffer %s" buffer))
+
+  (defun my-gptel--read_buffer (buffer)
+    (with-temp-message "Reading buffer"
+      (unless (buffer-live-p (get-buffer buffer))
+        (error "Error: buffer %s is not live." buffer))
+      (with-current-buffer  buffer
+        (buffer-substring-no-properties (point-min) (point-max)))))
+
+  (defun my-gptel--make_directory (parent name)
+    (condition-case nil
+        (progn
+          (make-directory (expand-file-name name parent) t)
+          (format "Directory %s created/verified in %s" name parent))
+      (error (format "Error creating directory %s in %s" name parent)))))
 
 ;; Holds a list of tools available for LLM to use
 ;; @see https://github.com/karthink/gptel/issues/514
@@ -40,19 +64,14 @@
                      (shr-insert-document dom)
                      (buffer-substring-no-properties (point-min) (point-max))))))
    :name "read_url"
-   :description "Fetch and read the contents of a URL"
+   :description "Fetch and read the contents of a URL."
    :args (list '(:name "url"
                        :type "string"
                        :description "The URL to read"))
    :category "web")
 
   (gptel-make-tool
-   :function (lambda (buffer text)
-               (with-current-buffer (get-buffer-create buffer)
-                 (save-excursion
-                   (goto-char (point-max))
-                   (insert text)))
-               (format "Appended text to buffer %s" buffer))
+   :function #'my-gptel--append_to_buffer
    :name "append_to_buffer"
    :description "Append text to the an Emacs buffer.  If the buffer does not exist, it will be created."
    :args (list '(:name "buffer"
@@ -63,53 +82,28 @@
                        :description "The text to append to the buffer."))
    :category "emacs")
 
-  ;; Message buffer logging tool
   (gptel-make-tool
-   :function (lambda (text)
-               (message "%s" text)
-               (format "Message sent: %s" text))
-   :name "echo_message"
-   :description "Send a message to the *Messages* buffer"
-   :args (list '(:name "text"
-                       :type "string"
-                       :description "The text to send to the messages buffer"))
-   :category "emacs")
-
-  ;; buffer retrieval tool
-  (gptel-make-tool
-   :function (lambda (buffer)
-               (unless (buffer-live-p (get-buffer buffer))
-                 (error "Error: buffer %s is not live." buffer))
-               (with-current-buffer  buffer
-                 (buffer-substring-no-properties (point-min) (point-max))))
+   :function #'my-gptel--read_buffer
    :name "read_buffer"
-   :description "Return the contents of an Emacs buffer"
+   :description "Return the contents of an Emacs buffer."
    :args (list '(:name "buffer"
                        :type "string"
                        :description "The name of the buffer whose contents are to be retrieved"))
    :category "emacs")
 
   (gptel-make-tool
-   :function (lambda (directory)
-               (mapconcat #'identity
-                          (directory-files directory)
-                          "\n"))
+   :function (lambda (directory) (mapconcat #'identity (directory-files directory) "\n"))
    :name "list_directory"
-   :description "List the contents of a given directory"
+   :description "List the contents of a given directory."
    :args (list '(:name "directory"
                        :type "string"
                        :description "The path to the directory to list"))
    :category "filesystem")
 
   (gptel-make-tool
-   :function (lambda (parent name)
-               (condition-case nil
-                   (progn
-                     (make-directory (expand-file-name name parent) t)
-                     (format "Directory %s created/verified in %s" name parent))
-                 (error (format "Error creating directory %s in %s" name parent))))
+   :function #'my-gptel--make_directory
    :name "make_directory"
-   :description "Create a new directory with the given name in the specified parent directory"
+   :description "Create a new directory with the given name in the specified parent directory."
    :args (list '(:name "parent"
                        :type "string"
                        :description "The parent directory where the new directory should be created, e.g. /tmp")
@@ -121,7 +115,7 @@
   (gptel-make-tool
    :function #'my-gptel--create_file
    :name "create_file"
-   :description "Create a new file with the specified content"
+   :description "Create a new file with the specified content."
    :args (list '(:name "path"
                        :type "string"
                        :description "The directory where to create the file")
@@ -136,7 +130,7 @@
   (gptel-make-tool
    :function #'my-gptel--read_file
    :name "read_file"
-   :description "Read and display the contents of a file"
+   :description "Read and display the contents of a file."
    :args (list '(:name "filepath"
                        :type "string"
                        :description "Path to the file to read.  Supports relative paths and ~."))
@@ -154,7 +148,7 @@
   (gptel-make-tool
    :function #'my-gptel--run_script
    :name "run_script"
-   :description "Run script"
+   :description "Run the script along with its specified arguments using the program."
    :args (list
           '(:name "script_program"
                   :type "string"
@@ -172,7 +166,8 @@
                (with-temp-message (format "Running command: %s" command)
                  (shell-command-to-string command)))
    :name "run_command"
-   :description "Run command"
+
+   :description "Run command."
    :args (list
           '(:name "command"
                   :type "string"
