98fee9c4d72f6b68814c20aae3e9c037e0b2d420
陈虎明 <chenhuming@gmail.com>
2025-12-24 14:10:10 +0800

    Use eglot 1.19 for  call and type hierarchies.

--- .emacs.d/elisp/init-eglot.el
+++ .emacs.d/elisp/init-eglot.el
@@ -15,6 +15,10 @@
 ;;  eglot-rename
 
 
+;; ensure 1.19 installed since it added call and type hierarchies.
+;; @see https://cgit.git.savannah.gnu.org/cgit/emacs.git/commit/?id=1ef9de69b3c3d8254ab58bf455137a4439dce516
+(require-package 'eglot "1.19")
+
 ;; by default, clangd will be used as c/c++ lsp server
 (use-package eglot
   :ensure t
@@ -26,10 +30,22 @@
         eglot-events-buffer-size 0
         eglot-send-changes-idle-time 0.5)
   :config
-  (use-package consult-eglot
-    :ensure t
-    :bind (:map eglot-mode-map
-                ("M-?" . consult-eglot-symbols))))
+  (progn
+    ;; update settings for clangd, run eglot-stderr-buffer to verify
+    (add-to-list 'eglot-server-programs
+                 '((c-mode c-ts-mode c++-mode c++-ts-mode objc-mode)
+                   . ("clangd"
+                      "--compile-commands-dir=./build/Linux_x86_64"
+                      "--background-index"
+                      "--clang-tidy"
+                      "--completion-style=detailed"
+                      "--header-insertion=never"
+                      "--pch-storage=memory"
+                      "--malloc-trim")))
+    (use-package consult-eglot
+      :ensure t
+      :bind (:map eglot-mode-map
+                  ("M-?" . consult-eglot-symbols)))))
 
 
 (provide 'init-eglot)
