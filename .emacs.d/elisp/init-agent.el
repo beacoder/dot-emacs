f2f1015309e5c79110be7dafb4724bcc1232b45d
陈虎明 <chenhuming@gmail.com>
2026-04-15 17:39:52 +0800

    Remove grep since it's been merged into gptel-agent already.

--- .emacs.d/elisp/init-agent.el
+++ .emacs.d/elisp/init-agent.el
@@ -96,67 +96,11 @@
       (gptel-agent--truncate-buffer "glob")
       (buffer-string))))
 
-(defun gptel-agent--git-grep (regex path &optional glob context-lines)
-  (unless (file-readable-p path)
-    (error "Error: File or directory %s is not readable" path))
-  (let ((grepper (or (executable-find "rg") (executable-find "grep"))))
-    (unless grepper
-      (error "Error: ripgrep/grep not available, this tool cannot be used"))
-    (with-temp-buffer
-      (let* ((full-path (expand-file-name (substitute-in-file-name path)))
-             (git-root (and (executable-find "git") (locate-dominating-file full-path ".git")))
-             (cmd (file-name-sans-extension (file-name-nondirectory grepper)))
-             (grepper (if git-root "git" grepper))
-             (default-directory (or git-root default-directory))
-             (args
-              (cond
-               (git-root
-                (delq nil (append
-                           (list "grep"
-                                 "--line-number"
-                                 "--no-color"
-                                 (and (natnump context-lines)
-                                      (format "-C%d" context-lines))
-                                 "--max-count=1000"
-                                 "--untracked"
-                                 ;; pcre regex
-                                 "-P" regex
-                                 "--")
-                           ;; glob restriction
-                           (when glob
-                             (list (format "%s" glob))))))
-               ((string= "rg" cmd)
-                (delq nil (list "--sort=modified"
-                                (and (natnump context-lines)
-                                     (format "--context=%d" context-lines))
-                                (and glob (format "--glob=%s" glob))
-                                ;; "--files-with-matches"
-                                "--max-count=1000"
-                                "--heading" "--line-number" "-e" regex
-                                full-path)))
-               ((string= "grep" cmd)
-                (delq nil (list "--recursive"
-                                (and (natnump context-lines)
-                                     (format "--context=%d" context-lines))
-                                (and glob (format "--include=%s" glob))
-                                "--max-count=1000"
-                                "--line-number" "--regexp" regex
-                                full-path)))
-               (t (error "Error: failed to identify grepper"))))
-             (exit-code (apply #'call-process grepper nil '(t t) nil args)))
-        (when (/= exit-code 0)
-          (goto-char (point-min))
-          (insert (format "Error: search failed with exit-code %d.  Tool output:\n\n" exit-code)))
-        (gptel-agent--truncate-buffer "grep")
-        (buffer-string)))))
-
 (use-package gptel-agent
   :ensure t
   :config
   (progn
     (gptel-agent-update)
-    ;; use git-grep since it's much faster in git repo
-    (defalias 'gptel-agent--grep 'gptel-agent--git-grep)
     ;; use git-ls-files since it's much faster in git repo
     (defalias 'gptel-agent--glob 'gptel-agent--git-glob)
     ;; add project related information as llm context, e.g: coding guideline, etc.
