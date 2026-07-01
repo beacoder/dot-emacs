b27edd442b0dd0359a82f14755362959a66b946e
陈虎明 <chenhuming@gmail.com>
2026-07-01 10:22:54 +0800

    Add headers.

--- .emacs.d/site-lisp/gptel-agent-loop.el
+++ .emacs.d/site-lisp/gptel-agent-loop.el
@@ -1,5 +1,30 @@
 ;;; gptel-agent-loop.el --- Prevent gptel agent from stopping prematurely -*- lexical-binding: t -*-
 
+;; Copyright (C) 2026 Huming Chen
+
+;; Author: Huming Chen <chenhuming@gmail.com>
+;; Maintainer: Huming Chen <chenhuming@gmail.com>
+;; URL: https://github.com/beacoder/dot-emacs/blob/master/.emacs.d/site-lisp/gptel-agent-loop.el
+;; Version: 0.1
+;; Keywords: programming, convenience
+;; Created: 2026-06-30
+;; Package-Requires: ((emacs "26.1"))
+;;
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Prevents gptel's agentic loop from stopping without confirming task
