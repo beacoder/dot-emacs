20134c73d80068f4723d24c4062f4ecd65dc8102
陈虎明 <chenhuming@gmail.com>
2026-07-14 11:08:07 +0800

    Update test-startup.sh

--- test-startup.sh
+++ test-startup.sh
@@ -7,7 +7,7 @@ ${EMACS:=emacs} -nw --batch \
                               (url-show-status nil)
                               (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
-                              (load-path (delq default-directory load-path)))
+                              (load-path (cons default-directory load-path)))
                            (setq package-check-signature nil)
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook))))'
