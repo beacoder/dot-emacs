7d66d02b9d0625f5fde05670ec668eb4399c099d
陈虎明 <chenhuming@gmail.com>
2026-07-16 10:21:56 +0800

    Adding embark-consult and improve init-minibuffer.el

--- elisp/init-minibuffer.el
+++ elisp/init-minibuffer.el
@@ -2,10 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; vertical completion
-(use-package vertico
-  :ensure t
-  :hook (after-init . vertico-mode))
 
 ;; Optionally use the `orderless' completion style.
 (use-package orderless
@@ -15,42 +11,58 @@
   (completion-category-overrides '((file (styles basic partial-completion))))
   (orderless-component-separator #'orderless-escapable-split-on-space))
 
-;; add extra action to minibuffer command
-(use-package embark
-  :ensure t
-  :bind
-  (("C-." . embark-act)) ;; pick some comfortable binding
-  :init
-  (setq prefix-help-command #'embark-prefix-help-command)
-  :config
-  ;; Hide the mode line of the Embark live/completions buffers
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
+(use-package consult-flycheck :ensure t)
 
-(when (maybe-require-package 'consult)
-  (defmacro sanityinc/no-consult-preview (&rest cmds)
-    `(with-eval-after-load 'consult
-       (consult-customize ,@cmds :preview-key "M-P")))
+;; vertical completion
+(when (maybe-require-package 'vertico)
+  (add-hook 'after-init-hook 'vertico-mode)
 
-  (sanityinc/no-consult-preview
-   consult-ripgrep
-   consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult-source-recent-file consult-source-project-recent-file consult-source-bookmark)
+  (when (maybe-require-package 'embark)
+    (with-eval-after-load 'vertico
+      (define-key vertico-map (kbd "C-c C-o") 'embark-export)
+      (define-key vertico-map (kbd "C-c C-c") 'embark-act)
+      (define-key vertico-map (kbd "C-c C-l") 'embark-collect)))
 
-  (global-set-key [remap switch-to-buffer] 'consult-buffer)
-  (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
-  (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
-  (global-set-key [remap goto-line] 'consult-goto-line)
+  ;; https://github.com/purcell/whole-line-or-region/issues/30#issuecomment-3388095018
+  (with-eval-after-load 'embark
+    (push 'embark--mark-target
+          (alist-get 'whole-line-or-region-delete-region
+                     embark-around-action-hooks)))
 
-  (use-package consult-flycheck :ensure t))
+  (when (maybe-require-package 'embark-consult)
+    (require 'embark-consult)
+    (defmacro sanityinc/no-consult-preview (&rest cmds)
+      `(with-eval-after-load 'consult
+         (consult-customize ,@cmds :preview-key "M-P")))
+
+    (setq xref-show-xrefs-function 'consult-xref)
+
+    (sanityinc/no-consult-preview
+     consult-ripgrep
+     consult-git-grep consult-grep
+     consult-bookmark consult-recent-file consult-xref
+     consult-source-recent-file consult-source-project-recent-file consult-source-bookmark)
+
+    (defun sanityinc/consult-ripgrep-at-point (&optional dir initial)
+      (interactive (list current-prefix-arg
+                         (if (use-region-p)
+                             (buffer-substring-no-properties
+                              (region-beginning) (region-end))
+                           (if-let* ((s (symbol-at-point)))
+                               (symbol-name s)))))
+      (consult-ripgrep dir initial))
+    (sanityinc/no-consult-preview sanityinc/consult-ripgrep-at-point)
+    (when (executable-find "rg")
+      (global-set-key (kbd "M-?") 'sanityinc/consult-ripgrep-at-point))
+
+    (global-set-key [remap switch-to-buffer] 'consult-buffer)
+    (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
+    (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
+    (global-set-key [remap goto-line] 'consult-goto-line)))
 
 ;; show minibuffer command docs
-(use-package marginalia
-  :ensure t
-  :hook (after-init . marginalia-mode))
+(when (maybe-require-package 'marginalia)
+  (add-hook 'after-init-hook 'marginalia-mode))
 
 
 (provide 'init-minibuffer)
